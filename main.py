e561281a4735cc58d6b67d21e16ee7ca03744989
peng <pengod4150@gmail.com>
2017-10-26 19:35:38 +0800

    box muller

---
+++ main.py
@@ -0,0 +1,45 @@
+import random_data_generator as rdg
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+import sys
+
+
+num_data = 5000
+num_histograms = 10
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: ", sys.argv[0], "<mean> ", "variance")
+        print("Use default normal distribution: mean=0, var=1")
+        mean = 0
+        var = 1
+    else:
+        mean = float(sys.argv[1])
+        var = float(sys.argv[2])
+
+    x = np.zeros(num_data)
+    for i in range(num_data):
+        x[i] = rdg.box_muller(mean, var)
+
+    x_min = mean - 4 * var
+    x_max = mean + 4 * var
+
+    # histogram
+    mu, sigma = mean, var
+
+    # the histogram of the data
+    n, bins, patches = plt.hist(x, 100, normed=1, facecolor='green', alpha=0.75)
+
+    # add a 'best fit' line (red line)
+    y = mlab.normpdf(bins, mu, sigma)
+    l = plt.plot(bins, y, 'r--', linewidth=1)
+
+    plt.xlabel('Data')
+    plt.ylabel('Probability')
+    plt.title(r'$\mathrm{Histogram\ of\ data:}\ \mu=%.2f,\ \sigma=%.2f$' % (mu, sigma))
+    plt.axis([x_min, x_max, 0, 1])
+    plt.grid(True)
+
+    plt.show()
---
+++ random_data_generator.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+import sys
+
+
+generate = 0
+z1 = 0
+
+
+def box_muller(mean, var):
+    """
+    Box-Muller transform - Generate Gaussian data
+    """
+    global generate
+    global z1
+
+    generate = ~generate
+
+    if not generate:
+        return mean + z1 * var
+
+    u, v = 0.0, 0.0
+    while u < sys.float_info.epsilon:
+        u = np.random.random()
+        v = np.random.random()
+
+    z0 = np.sqrt(-2 * np.log(u)) * np.cos(2 * np.pi * v)
+    z1 = np.sqrt(-2 * np.log(u)) * np.sin(2 * np.pi * v)
+    return mean + z0 * var
+
+
